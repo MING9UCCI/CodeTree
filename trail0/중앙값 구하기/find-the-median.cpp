3dab5eabe82c1db036dbde87662e273c85714b7a
Jhee Minwoo <64111159+MING9UCCI@users.noreply.github.com>
2026-07-15 22:29:35 +0900

    [Passed] 중앙값 구하기 (2ms, 0MB)

---
+++ trail0/중앙값 구하기/find-the-median.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A, B, C;
+    cin >> A >> B >> C;
+    if (A >= B) { 
+        if (A >= C) {
+            if (B >= C) {
+                cout << B;
+            } else {
+                cout << C;
+            }
+        } else {
+            cout << A;
+        }
+    } else {
+        if (B >= C) {
+            if (A >= C) {
+                cout << A;
+            } else {
+                cout << C;
+            }
+        } else {
+            cout << B;
+        }
+    }
+    return 0;
+}
